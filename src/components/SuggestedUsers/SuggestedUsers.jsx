81abf7c1b70a0a3aa920d748361697010393fa27
King Wu <154056545+Kingwu12@users.noreply.github.com>
2024-07-19 19:36:11 +1000

    com

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -29,7 +29,7 @@ const SuggestedUsers = () => {
       ))}
 
       <Box fontSize={12} color={'gray.500'} mt={5} alignSelf={'start'}>
-        © 2023 Built By{' '}
+        © 2024 Built By{' '}
         <Link href='https://www.kingwu.net/' target='_blank' color='blue.500' fontSize={14}>
           King Wu
         </Link>
